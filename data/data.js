a510b20cd90d43b92b08ac453ceec619235e7d6e
Kartik Dixit <kartikdixit2468@gmail.com>
2026-01-05 09:10:21 +0530

    Added seperate folder for storing pooja data

---
+++ data/data.js
@@ -0,0 +1,17 @@
+// Expanded Database of Poojas
+
+import React from 'react';
+import {
+    Flame,
+    Home,
+    Heart,
+    Sparkles,
+    Sun,
+    Baby,
+    Moon,
+    Star,
+    BookOpen
+} from 'lucide-react';
+
+
+export { ALL_POOJAS, FEATURED_SERVICES, REVIEWS };
